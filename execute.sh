667eb799c2c0b50a9400e5711ea2b9641950b25c
martin-illarion <martin.polak@gmx.at>
2021-07-29 19:29:36 +0000

    debug & update gemeinden

--- execute.sh
+++ execute.sh
@@ -17,8 +17,7 @@ dat=$(date)
 echo "$dt [INFO] BBG done"
 
 curl https://info.gesundheitsministerium.gv.at/data/impfungen-gemeinden.csv -o /home/ubuntu/ages_vac/ages_vac/gemeinden_tmp.csv
-tail -n +2 /home/ubuntu/ages_vac/ages_vac/gemeinden.csv >> /home/ubuntu/ages_vac/ages_vac/gemeinden_new.csv
-mv -f /home/ubuntu/ages_vac/ages_vac/gemeinden_new.csv /home/ubuntu/ages_vac/ages_vac/gemeinden.csv
+tail -n +2 /home/ubuntu/ages_vac/ages_vac/gemeinden_tmp.csv >> /home/ubuntu/ages_vac/ages_vac/gemeinden.csv
 
 git commit -am "$dat"
 echo "git commit done $dat"
