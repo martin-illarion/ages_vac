56b5f2619481aa2b9292d06c6a0f53613ceeceae
martin-illarion <martin.polak@gmx.at>
2021-12-22 18:42:11 +0000

    Wed Dec 22 18:41:55 UTC 2021 Manual

--- vac_bezirke.py
+++ vac_bezirke.py
@@ -8,7 +8,8 @@ l_file="/home/ubuntu/ages_vac/ages_vac/impfungen-gemeinden.csv"
 out_path="/home/ubuntu/ages_vac/ages_vac/"
 #out_path="C:\\Users\\mpolak_cloudbees\\Dropbox\\python\\ages_impfung\\"
 
-url = 'https://info.gesundheitsministerium.gv.at/data/impfungen-gemeinden.csv'
+#url = 'https://info.gesundheitsministerium.gv.at/data/impfungen-gemeinden.csv'
+url = 'https://info.gesundheitsministerium.gv.at/data/COVID19_vaccination_municipalities.csv'
 req = requests.get(url, allow_redirects=True)
 
 url_content = req.content
@@ -141,39 +142,46 @@ gkz_list = {
 pop_bez={}
 frst_bez={}
 scnd_bez={}
+thrd_bez={}
 
 with open(l_file,"r", newline='',encoding="utf-8-sig") as csvfile:
     cov_reader = csv.DictReader(csvfile, delimiter=';')
     for row in cov_reader:
-        #Datum;Gemeindecode;Bevölkerung;Teilgeimpfte;TeilgeimpftePro100;Vollimmunisierte;VollimmunisiertePro100
-        date_str = row["Datum"]
+        #date;municipality_id;municipality_name;municipality_population;dose_1;dose_2;dose_3;valid_certificates;valid_certificates_percent
+        date_str = row["date"]
         date_obj = datetime.strptime(date_str[0:10], "%Y-%m-%d")
         date = date_obj.strftime("%Y/%m/%d")
-        gkz = row["Gemeindecode"][0:3]
-        pop = row["Bevölkerung"]
-        frst = row["Teilgeimpfte"]
-        scnd = row["Vollimmunisierte"]
+        gkz = row["municipality_id"][0:3]
+        pop = row["municipality_population"]
+        frst = row["dose_1"]
+        scnd = row["dose_2"]
+        thrd = row["dose_3"]
         bl=bl_list[int(gkz[0])-1]
         if date not in pop_bez:
             pop_bez[date] = {}
             frst_bez[date] = {}
             scnd_bez[date] = {}
+            thrd_bez[date] = {}
         if bl not in pop_bez[date]:
             pop_bez[date][bl] = {}
             frst_bez[date][bl] = {}
             scnd_bez[date][bl] = {}
+            thrd_bez[date][bl] = {}
         if gkz not in pop_bez[date][bl]:
             pop_bez[date][bl][gkz] = int(pop)
             frst_bez[date][bl][gkz] = int(frst)
             scnd_bez[date][bl][gkz] = int(scnd)
+            thrd_bez[date][bl][gkz] = int(thrd)
         else:
             pop_bez[date][bl][gkz] += int(pop)
             frst_bez[date][bl][gkz] += int(frst)
             scnd_bez[date][bl][gkz] += int(scnd)
+            thrd_bez[date][bl][gkz] += int(thrd)
 
 hdr_pop=True
 hdr_teil=True
 hdr_voll=True
+hdr_boost=True
 
 with open(out_path + "vac_bez_pop.csv","r", encoding='utf-8') as f:
     if f.readline()[0:5]=="date,": hdr_pop=False
@@ -181,31 +189,38 @@ with open(out_path + "vac_bez_teil.csv","r", encoding='utf-8') as f:
     if f.readline()[0:5]=="date,": hdr_teil=False
 with open(out_path + "vac_bez_voll.csv","r", encoding='utf-8') as f:
     if f.readline()[0:5]=="date,": hdr_voll=False
+with open(out_path + "vac_bez_boost.csv","r", encoding='utf-8') as f:
+    if f.readline()[0:5]=="date,": hdr_boost=False
 
 with open(out_path + "vac_bez_pop.csv","a+", encoding='utf-8') as csv_popfile:
     with open(out_path + "vac_bez_teil.csv","a+", encoding='utf-8') as csv_teilfile:
         with open(out_path + "vac_bez_voll.csv","a+", encoding='utf-8') as csv_vollfile:
-            hdr = "date"
-            line_pop = date
-            line_frst = date
-            line_scnd = date
-            for bl in bl_list:
-                for d in pop_bez:
-                    for g in pop_bez[d][bl]:
-                        line_pop += "," + str(pop_bez[d][bl][g])
-                        line_frst += "," + str(frst_bez[d][bl][g])
-                        line_scnd += "," + str(scnd_bez[d][bl][g])
-                        hdr += "," + gkz_list[g]
-            hdr += "\n"
-            line_pop += "\n"
-            line_frst += "\n"
-            line_scnd += "\n"
-            if hdr_pop: csv_popfile.write(hdr)
-            if hdr_teil: csv_teilfile.write(hdr)
-            if hdr_voll: csv_vollfile.write(hdr)
-            csv_popfile.write(line_pop)
-            csv_teilfile.write(line_frst)
-            csv_vollfile.write(line_scnd)
+            with open(out_path + "vac_bez_boost.csv","a+", encoding='utf-8') as csv_boostfile:
+                hdr = "date"
+                line_pop = date
+                line_frst = date
+                line_scnd = date
+                line_thrd = date
+                for bl in bl_list:
+                    for d in pop_bez:
+                        for g in pop_bez[d][bl]:
+                            line_pop += "," + str(pop_bez[d][bl][g])
+                            line_frst += "," + str(frst_bez[d][bl][g])
+                            line_scnd += "," + str(scnd_bez[d][bl][g])
+                            line_thrd += "," + str(thrd_bez[d][bl][g])
+                            hdr += "," + gkz_list[g]
+                hdr += "\n"
+                line_pop += "\n"
+                line_frst += "\n"
+                line_scnd += "\n"
+                line_thrd += "\n"
+                if hdr_pop: csv_popfile.write(hdr)
+                if hdr_teil: csv_teilfile.write(hdr)
+                if hdr_voll: csv_vollfile.write(hdr)
+                if hdr_boost: csv_boostfile.write(hdr)
+                csv_popfile.write(line_pop)
+                csv_teilfile.write(line_frst)
+                csv_vollfile.write(line_scnd)
 
 #with open(out_path + bl + "_bez.csv","w", encoding='utf-8') as csv_outfile:
 
